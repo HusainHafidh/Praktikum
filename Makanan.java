df49a1d52af2530549943d3e62016e650342cdfe
HusainHafidh <husain092003@gmail.com>
2023-03-09 19:04:17 +0700

    Praktikum

---
+++ Makanan.java
@@ -0,0 +1,11 @@
+package strukturdata;
+
+/**
+ *
+ * @author USER
+ */
+public class Makanan extends Hidangan{
+    public String disantap(){
+        return this.getNamaHidangan() + " dimakan";
+    }
+}
